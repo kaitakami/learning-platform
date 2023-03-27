16ee8cd48ab7c2e0fe16b330144d2f7e490e0b73
Kai Takami <kaitakami@protonmail.com>
2023-03-27 13:36:59 +0900

    Add next theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes"
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider>
+          <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 };

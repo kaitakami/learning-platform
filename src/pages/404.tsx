ed7d522c00b4e419a481aed39c6ce578cd40bdce
Kai Takami <kaitakami@protonmail.com>
2023-03-29 18:14:43 +0900

    Create 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/marketing/Layout'
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <div className='items-center w-full flex justify-center min-h-screen grid-pattern'>
+        <h1 className='lg:text-7xl md:text-6xl text-4xl font-bold'>Error 404</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default ErrorPage

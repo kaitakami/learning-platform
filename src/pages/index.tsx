d07cc52c89818283ebdf3bcc5f7ee37a17d141dd
Kai Takami <kaitakami@protonmail.com>
2023-03-31 20:53:01 +0900

    Add initial toast message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from 'react';
 import { type NextPage } from "next";
 import { motion } from "framer-motion"
 import Layout from "../components/marketing/Layout";
 import Link from "next/link";
 import { buttonVariants } from "../components/ui/button";
 import { Button } from "../components/ui/button";
-import { ArrowRight, Briefcase, Gamepad, Keyboard, PiggyBank, Ship, Timer } from "lucide-react"
+import { Github, ArrowRight, Briefcase, Gamepad, Keyboard, PiggyBank, Ship, Timer } from "lucide-react"
+import { toast } from 'react-hot-toast';
+
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    function githubStarToast() {
+      toast(<>If you like Enzan Learn, please star it on Github! <Link href="https://github.com/kaitakami" target="_blank" className={buttonVariants()}><Github /></Link></>, { duration: 4000, position: "bottom-center", icon: "✨" })
+    }
+
+    setTimeout(() => {
+      githubStarToast()
+    }, 4000)
+  }, [])
   return (
-    <Layout>
+    <Layout navDelay={2}>
       <>
         <HeroSection />
       </>
@@ -24,7 +36,7 @@ import { signIn } from 'next-auth/react';
 const HeroSection = () => {
   return (
     <section className="grid-pattern min-h-screen flex items-center">
-      <div className="grid max-w-screen-xl px-4 py-20 mx-auto lg:gap-8 md:py-20 xl:gap-16 lg:grid-cols-12">
+      <div className="grid max-w-screen-xl px-4 py-24 mx-auto lg:gap-8 md:py-20 xl:gap-16 lg:grid-cols-12">
         <div className="max-w-4xl mr-auto place-self-center lg:col-span-7 xl:col-span-8 md:col-span-6">
           <motion.h1
             className="mb-4 text-4xl sm:text-5xl font-extrabold tracking-tight leading-none xl:text-6xl bg-gradient-to-tr dark:from-gray-100 dark:to-slate-300 from-slate-700 to-slate-900 bg-clip-text text-transparent capitalize"
@@ -83,7 +95,7 @@ const AnimatedCard = () => {
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
+      initial={{ opacity: 0, willChange: "opacity" }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8, delay: 2 }}
       ref={ref}
@@ -92,12 +104,13 @@ const AnimatedCard = () => {
       onMouseOut={() => setMouseOverCard(false)}
       className="md:h-[30rem] md:w-[23rem] sm:w-80 h-96 w-full transition-all shadow-xl hover:shadow-2xl shadow-stone-300/30 hover:shadow-stone-300 bg-stone-200/90 dark:bg-zinc-900 dark:shadow-stone-900/70 dark:hover:shadow-stone-900/80 flex flex-col mx-auto border-gradient"
       style={{
-        transform: mouseOverCard ? `perspective(500px) scale(1.1) rotateX(${cardRotation.x}deg) rotateY(${cardRotation.y}deg)` : "perspective(500px) scale(1) rotateX(0deg) rotateY(0deg)"
+        transform: mouseOverCard ? `perspective(500px) scale(1.1) rotateX(${cardRotation.x}deg) rotateY(${cardRotation.y}deg)` : "perspective(500px) scale(1) rotateX(0deg) rotateY(0deg)",
+        willChange: "opacity, transform"
       }}
     >
       <ul className="flex flex-col justify-between h-full py-10 px-8">
         <li className="flex gap-2 my-auto md:text-xl"><Gamepad />Gamified Experience</li>
-        <li className="flex gap-2 my-auto md:text-xl"><PiggyBank />Free in Beta Version </li>
+        <li className="flex gap-2 my-auto md:text-xl"><PiggyBank />Free in Beta Version</li>
         <li className="flex gap-2 my-auto md:text-xl"><Keyboard />Learn by doing</li>
         <li className="flex gap-2 my-auto md:text-xl"><Timer />Divided in short classes</li>
         <li className="flex gap-2 my-auto md:text-xl"><Briefcase />Build your Portfolio</li>

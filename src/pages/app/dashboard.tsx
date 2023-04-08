f2176a98646548e3c9ba1bcd03752c566093e901
Kai Takami <kaitakami@protonmail.com>
2023-04-08 23:29:26 +0900

    Create dashboard sidebar

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,142 +1,100 @@
 import Layout from "@/src/components/app/Layout"
-import {
-  Card,
-  Table,
-  TableRow,
-  TableCell,
-  TableHead,
-  TableHeaderCell,
-  TableBody,
-  BadgeDelta,
-  type DeltaType,
-} from "@tremor/react";
-
-const salesPeople: {
-  name: string;
-  leads: number;
-  sales: string;
-  quota: string;
-  variance: string;
-  region: string;
-  delta: string;
-  deltaType: DeltaType;
-}[] = [
-    {
-      name: "Peter Doe",
-      leads: 45,
-      sales: "1,000,000",
-      quota: "1,200,000",
-      variance: "low",
-      region: "Region A",
-      delta: "overperforming",
-      deltaType: "moderateIncrease",
-    },
-    {
-      name: "Lena Whitehouse",
-      leads: 35,
-      sales: "900,000",
-      quota: "1,000,000",
-      variance: "low",
-      region: "Region B",
-      delta: "average",
-      deltaType: "unchanged",
-    },
-    {
-      name: "Phil Less",
-      leads: 52,
-      sales: "930,000",
-      quota: "1,000,000",
-      variance: "medium",
-      region: "Region C",
-      delta: "underperforming",
-      deltaType: "moderateDecrease",
-    },
-    {
-      name: "John Camper",
-      leads: 22,
-      sales: "390,000",
-      quota: "250,000",
-      variance: "low",
-      region: "Region A",
-      delta: "overperforming",
-      deltaType: "increase",
-    },
-    {
-      name: "Max Balmoore",
-      leads: 49,
-      sales: "860,000",
-      quota: "750,000",
-      variance: "low",
-      region: "Region B",
-      delta: "overperforming",
-      deltaType: "increase",
-    },
-    {
-      name: "Peter Moore",
-      leads: 82,
-      sales: "1,460,000",
-      quota: "1,500,000",
-      variance: "low",
-      region: "Region A",
-      delta: "average",
-      deltaType: "unchanged",
-    },
-    {
-      name: "Joe Sachs",
-      leads: 49,
-      sales: "1,230,000",
-      quota: "1,800,000",
-      variance: "medium",
-      region: "Region B",
-      delta: "underperforming",
-      deltaType: "moderateDecrease",
-    },
-  ];
+import { Separator } from "@/src/components/ui/separator"
+import { H1 } from "@/src/components/ui/headings";
 
 function Dashboard() {
   return (
     <Layout>
-      <Card>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeaderCell> Name </TableHeaderCell>
-              <TableHeaderCell className="text-right"> Leads </TableHeaderCell>
-              <TableHeaderCell className="text-right">
-                {" "}
-                Sales ($){" "}
-              </TableHeaderCell>
-              <TableHeaderCell className="text-right">
-                {" "}
-                Quota ($){" "}
-              </TableHeaderCell>
-              <TableHeaderCell className="text-right"> Variance </TableHeaderCell>
-              <TableHeaderCell className="text-right"> Region </TableHeaderCell>
-              <TableHeaderCell className="text-right"> Status </TableHeaderCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {salesPeople.map((item) => (
-              <TableRow key={item.name}>
-                <TableCell>{item.name}</TableCell>
-                <TableCell className="text-right">{item.leads}</TableCell>
-                <TableCell className="text-right">{item.sales}</TableCell>
-                <TableCell className="text-right">{item.quota}</TableCell>
-                <TableCell className="text-right">{item.variance}</TableCell>
-                <TableCell className="text-right">{item.region}</TableCell>
-                <TableCell className="text-right">
-                  <BadgeDelta deltaType={item.deltaType} size="xs">
-                    {item.delta}
-                  </BadgeDelta>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Card>
+      <div className="lg:grid grid-cols-10">
+        <motion.div
+          initial={{ opacity: 0, x: -40 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-3 xl:col-span-2 hidden lg:block h-screen overflow-y-auto bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-stone-100 to-stone-200 dark:from-stone-800 dark:to-stone-900 shadow-xl py-16 border-r border-stone-200 dark:border-stone-700"
+        >
+          <H1 className="px-4">Your learning progress ✨</H1>
+          <Separator className="mt-5 mb-10" />
+          <div className="flex flex-col px-5 gap-4">
+            <Course
+              title="Build fullstack web3 application with nextjs"
+              tags={[{ value: "Nextjs", color: "stone" }, { value: "Solidity", color: "amber" }, { value: "Web3" }, { value: "Typescript", color: "sky" }]}
+              startDate="2023-10-01"
+              dueDate="2023-10-31"
+              pointsLeft={128}
+              completedLessons={2}
+              totalLessons={3}
+            />
+            {/* ENROLL TO A NEW COURSE CTA */}
+            <Link href="#" className="w-full px-3 py-5 rounded-lg hover:shadow-lg dark:hover:shadow-neutral-900 hover:shadow-neutral-200
+            border-dashed border-2 dark:border-stone-700 border-stone-300 h-52 flex justify-center flex-col dark:hover:bg-stone-900 hover:bg-stone-100 transition-all">
+              <div>
+                <h2 className="text-3xl font-medium text-center text-stone-500 dark:text-stone-500">Enroll to a new course</h2>
+              </div>
+            </Link>
+          </div>
+        </motion.div>
+        <div>
+          <div>
+            <h2>Welcome to the page</h2>
+            <h3>User Name</h3>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export default Dashboard
+import { Calendar } from "lucide-react";
+import Link from "next/link";
+
+import { ProgressBar, Badge as Tag } from "@tremor/react"
+interface Tag {
+  size?: "sm" | "md" | "lg",
+  color?: "gray" | "slate" | "zinc" | "neutral" | "stone" | "red" | "orange" | "amber" | "yellow" | "lime" | "green" | "emerald" | "teal" | "cyan" | "sky" | "blue" | "indigo" | "violet" | "purple" | "fuchsia" | "pink" | "rose" | undefined,
+  value: string,
+}
+import { motion } from "framer-motion";
+
+const Course: React.FC<{
+  title: string,
+  tags: Tag[],
+  startDate: string,
+  dueDate: string,
+  pointsLeft: number,
+  totalLessons: number,
+  completedLessons: number
+}> = ({ title, tags, startDate, dueDate, pointsLeft, totalLessons, completedLessons }) => {
+
+  const percentageValue = Number(((completedLessons / totalLessons) * 100).toFixed(1))
+
+  return (
+    <Link href="#" className="w-full px-3 py-5 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-stone-100 to-gray-100 dark:from-stone-800 dark:to-stone-900 rounded-lg space-y-5 hover:shadow-lg dark:hover:shadow-neutral-900 hover:shadow-neutral-200 transition-shadow border dark:border-stone-700">
+      <div className="space-y-1">
+        <h2 className="text-xl font-medium">{title}</h2>
+        <div className="flex gap-1.5 flex-wrap">
+          {tags.map(({ size, color, value }) => (
+            <Tag key={value} size={size || "sm"} color={color || "fuchsia"}>
+              {value}
+            </Tag>
+          ))}
+        </div>
+      </div>
+      <div className="dark:text-stone-400 space-y-2 text-stone-700">
+        <div className="flex place-content-start space-x-2">
+          <Calendar className="w-5 h-5" /><span>{startDate} - {dueDate}</span>
+        </div>
+        <p>{pointsLeft} points to get ✅</p>
+        <div className="space-y-1">
+          <span className="dark:text-stone-300/70 text-sm">{completedLessons} / {totalLessons} classes &bull; {percentageValue}% </span>
+          <ProgressBar color="sky" className="pb-2" percentageValue={percentageValue} />
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+// Calendar showing last day
+// Progress graph
+// Show courses in order of last day (in other words look for the latests lessons made and show the course without repeating)
+// Updates?

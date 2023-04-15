83fb6ff833b9b38d2958557f9198e41b50fac6c7
Kai Takami <kaitakami@protonmail.com>
2023-04-15 23:01:56 +0900

    Create introduction page

---
+++ src/pages/introduction/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '@/src/components/marketing/Layout'
+
+const introduction = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Introduction</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default introduction

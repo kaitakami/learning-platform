f144d31fba51097f3410569cb989288fbdcb8f55
Kai Takami <kaitakami@protonmail.com>
2023-04-15 23:00:45 +0900

    Create gamification router

---
+++ src/server/api/routers/gamification.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const gamificationRouter = createTRPCRouter({
+  gamification: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input: { id } }) => {
+      return await ctx.prisma.gamification.findUnique({
+        where: {
+          userId: id,
+        },
+      });
+    }),
+});
